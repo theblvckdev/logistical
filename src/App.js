d0520d911450e8d3cd5d00c60dfd7b7a7f2c8be5
joeCodes05 <nathancodes05@gmail.com>
2023-11-02 07:16:43 -1100

    code update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home/home";
-import './assets/sass/global.scss'
+import "./assets/sass/global.scss";
 import PageNotFound from "./pages/page_not_found/pageNotFound";
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
 import { useEffect } from "react";
+import { Layout } from "./layout/layout";
+import About from "./pages/about/about";
 
 function App() {
   useEffect(() => {
     Aos.init();
     Aos.refresh();
   }, []);
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" Component={HomePage} />
-        <Route path="*" Component={PageNotFound} />
-      </Routes>
-    </Router>
-  );
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+      ],
+      errorElement: <PageNotFound />,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
