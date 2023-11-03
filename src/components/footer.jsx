ffdbcde63688d00f1b21b58dc4590a7d53dc6178
joeCodes05 <nathancodes05@gmail.com>
2023-11-02 23:48:24 -1100

    working on footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
       <footer className="w-full bg-white overflow-hidden py-[5rem]">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore odit
-        voluptatibus velit ad eos expedita. Distinctio quidem laborum omnis,
-        atque ipsum minima architecto eos ut libero explicabo nostrum nemo vel.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus
-        incidunt nam id, aliquam corporis placeat consectetur. Corrupti beatae,
-        iure quod magni a, sapiente perferendis dolores expedita, similique nam
-        optio.
+        <div className="flex gap-4 px-[20px] lg:px-[50px]">
+          <div className="basis-1/2">
+            <div>
+              <Link to="/" className="-m-1.5 p-1.5 text-gray-900">
+                <h1
+                  data-aos="fade-up"
+                  data-aos-duration="2000"
+                  className="text-3xl tracking-widest font-jost leading-[24px] font-normal uppercase">
+                  Logistica.
+                </h1>
+              </Link>
+
+              <p
+                data-aos="fade-up"
+                data-aos-duration="2000"
+                className="w-full font-jost md:w-8/12 text-gray-500 lg:leading-8 lg:text-lg">
+                Optimized logistics can reduce transportation, storage, and
+                inventory costs, leading to higher profitability.
+              </p>
+            </div>
+          </div>
+        </div>
       </footer>
     </>
   );
