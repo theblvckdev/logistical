7d34859a964b54cb18dc03fa4a11ce217af51ab4
joeCodes05 <nathancodes05@gmail.com>
2023-11-03 03:10:39 -1100

    code updates

---
+++ src/data/supportLinks.js
@@ -0,0 +1,5 @@
+export const supportLinks = [
+  { linkName: "Privacy policies", path: "/privacy-policies" },
+  { linkName: "FAQs", path: "/frequently-asked-questions" },
+  { linkName: "Terms & Conditions", path: "/terms-and-conditions" },
+];
