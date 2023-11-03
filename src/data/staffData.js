7f03e5fd478e40c831d615d2e732afccc9e74ce0
joeCodes05 <nathancodes05@gmail.com>
2023-11-02 23:48:49 -1100

    code update

--- src/data/staffData.js
+++ src/data/staffData.js
@@ -1,21 +1,21 @@
-import staffOne from '../assets/images/t-01.jpg'
-import staffTwo from '../assets/images/t-02.jpg'
-import staffThree from '../assets/images/t-03.jpg'
+import staffOne from "../assets/images/t-01.jpg";
+import staffTwo from "../assets/images/t-02.jpg";
+import staffThree from "../assets/images/t-03.jpg";
 
 export const staffData = [
   {
     staffName: "Alisson Taylor",
     staffPosition: "Team Manager",
-    imageUrl: staffOne
+    imageUrl: staffOne,
   },
   {
     staffName: "John Doe",
-    staffPosition: "CEO",
-    imageUrl: staffThree
+    staffPosition: "Chief Executive Officer",
+    imageUrl: staffThree,
   },
   {
     staffName: "Allen Iverson",
-    staffPosition: "CTO",
-    imageUrl: staffTwo
-  }
-]
+    staffPosition: "Project Manager",
+    imageUrl: staffTwo,
+  },
+];
