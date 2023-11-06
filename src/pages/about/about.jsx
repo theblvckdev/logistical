98ac4980267ab41fa4851eeec77af24cbb82944a
joeCodes05 <nathancodes05@gmail.com>
2023-11-05 21:33:28 -1100

    update about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,6 +3,9 @@ import Banner from "./components/banner";
 import "./assets/sass/about.scss";
 import ChooseUs from "../../components/chooseUs";
 import AboutSales from "./components/aboutSales";
+import OurStaff from "../../components/ourStaff";
+import ContactRef from "../../components/contactRef";
+import SalesTwo from "./components/salesTwo";
 
 const About = () => {
   return (
@@ -11,6 +14,9 @@ const About = () => {
         <Banner />
         <ChooseUs showLink={false} />
         <AboutSales />
+        <SalesTwo />
+        <OurStaff />
+        <ContactRef />
       </main>
     </>
   );
