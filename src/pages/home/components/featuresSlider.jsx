7ee8302dd3dbd617b1883fbd1b320f0bb8bd6a7c
joeCodes05 <nathancodes05@gmail.com>
2023-11-05 21:34:42 -1100

    update home component

--- src/pages/home/components/featuresSlider.jsx
+++ src/pages/home/components/featuresSlider.jsx
@@ -16,7 +16,7 @@ const FeaturesSlider = () => {
                 const { fistFeature, secondFeature } = features;
                 return (
                   <div key={index}>
-                    <h1 className="text-[86px] leading-[86px] xl:text-[102px] tracking-[1px] percentage_value font-bedasNeue font-bold xl:leading-[120px] text-white flex gap-[4rem]">
+                    <h1 className="text-7xl leading-[72px] xl:text-8xl tracking-[1px] percentage_value font-bedasNeue font-bold xl:leading-[110px] text-white flex gap-4">
                       {fistFeature} {secondFeature}
                     </h1>
                   </div>
--- src/pages/home/components/logisticsCapabilities.jsx
+++ src/pages/home/components/logisticsCapabilities.jsx
@@ -1,44 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import * as FaIcons from "react-icons/fa";
 import * as BsIcons from "react-icons/bs";
 import { featuresData } from "../../../data/featuresData";
 import { progressData } from "../../../data/progressData";
+import ExternalLink from "../../../components/externalLink";
 
 const LogisticsCapabilities = () => {
   return (
     <>
-      <section className="logistics_capabilities py-[5rem] bg-white overflow-hidden">
+      <section className="logistics_capabilities py-20 bg-white overflow-hidden">
         <div className="px-[20px] lg:px-[50px] w-full">
-          <div className="block md:flex gap-[5rem]">
-            <div className="content_wrap_one w-[100%] md:w-[60%]">
+          <div className="block md:flex gap-4">
+            <div className="content_wrap_one w-full md:w-7/12">
               <div>
-                <Link
-                  to={"/services"}
-                  data-aos="fade-right"
-                  data-aos-duration="2000"
-                  className="no-underline text-gray-900 font-jost relative w-fit before:absolute before:-bottom-[20%] before:left-0 before:h-[1px] before:bg-gray-900 before:w-full duration-500 hover:before:w-0 before:duration-500 font-[500] text-[20px] flex items-center gap-3">
-                  <span>All Services</span>
-                  <FaIcons.FaExternalLinkAlt />
-                </Link>
+                <ExternalLink
+                  linkPath={"/services"}
+                  linkText={"All Services"}
+                />
               </div>
               <h1
                 data-aos="fade-in"
                 data-aos-duration="2000"
-                className="text-gray-800 font-outfit mt-[50px] font-[700] text-[40px] leading-[40px] md:text-[66px] md:leading-[66px] xl:text-[114px] xl:leading-[114px]">
+                className="text-gray-800 font-outfit mt-14 font-bold text-5xl leading-10 md:text-6xl md:leading-[60px] xl:text-8xl xl:leading-[96px]">
                 Logistics <br /> Capabilities
               </h1>
               <hr className="mt-9" />
               <h3
                 data-aos="fade-up"
                 data-aos-duration="2000"
-                className="mt-5 text-[18px] font-jost leading-[18px] lg:text-[32px] lg:leading-[32px] text-gray-800 font-[400]">
+                className="mt-5 text-lg font-jost leading-5 lg:text-3xl lg:leading-8 text-gray-800 font-normal">
                 Delivering Possibilities, On Time
               </h3>
               <p
                 data-aos="fade-up"
                 data-aos-duration="2000"
-                className="mt-5 w-fit font-jost text-[16px] lg:text-[17px] text-gray-700">
+                className="mt-5 w-fit font-jost text-base lg:text-lg text-gray-700">
                 At Logistica, we are more than just a logistics company – we are
                 the architects of seamless supply chains, the navigators of
                 global trade, and the enablers of business growth.
@@ -46,22 +41,22 @@ const LogisticsCapabilities = () => {
               <div
                 data-aos="fade-up"
                 data-aos-duration="2000"
-                className="mt-[50px]">
+                className="mt-14">
                 {featuresData.map((features, index) => {
                   const { fistFeature, secondFeature } = features;
                   return (
                     <div
-                      className="md:grid grid-cols-2 font-jost block gap-[2rem] w-full"
+                      className="md:grid grid-cols-2 font-jost block gap-4 w-full"
                       key={index}>
                       <div>
                         <div className="flex gap-1 items-center">
-                          <BsIcons.BsPlus className="text-[1.2rem] text-gray-700" />
+                          <BsIcons.BsPlus className="text-sm text-gray-700" />
                           <p className="text-gray-700">{fistFeature}</p>
                         </div>
                       </div>
                       <div>
                         <div className="flex gap-1 items-center">
-                          <BsIcons.BsPlus className="text-[1.2rem] text-gray-700" />
+                          <BsIcons.BsPlus className="text-sm text-gray-700" />
                           <p className="text-gray-700">{secondFeature}</p>
                         </div>
                       </div>
@@ -70,8 +65,8 @@ const LogisticsCapabilities = () => {
                 })}
               </div>
             </div>
-            <div className="content_wrap_two text-center md:text-left w-[100%] md:w-[40%]">
-              <div className="progress_wrap mt-5 lg:pl-[3rem]">
+            <div className="content_wrap_two text-center md:text-left w-full md:w-1/2">
+              <div className="progress_wrap mt-5 lg:pl-12">
                 {progressData.map((progress, index) => {
                   const { title, percentage } = progress;
                   return (
@@ -80,13 +75,13 @@ const LogisticsCapabilities = () => {
                       data-aos="fade-left"
                       data-aos-duration="2000"
                       className="flex flex-col items-center">
-                      <div className="text-gray-800 mt-[50px] w-full">
-                        <h1 className="text-[86px] font-bedasNeue font-bold leading-[86px] xl:text-[102px] -tracking-[1px] percentage_value xl:leading-[102px]">
+                      <div className="text-gray-800 mt-14 w-full">
+                        <h1 className="text-7xl font-bedasNeue font-bold leading-[72px] xl:text-8xl -tracking-[1px] percentage_value xl:leading-[96px]">
                           {percentage}%
                         </h1>
                         <hr className="mt-3" />
                         <div className="mt-3">
-                          <h3 className="text-gray-800 font-jost font-[500] text-[1.3rem]">
+                          <h3 className="text-gray-800 font-jost font-normal text-xl">
                             {title}
                           </h3>
                         </div>
