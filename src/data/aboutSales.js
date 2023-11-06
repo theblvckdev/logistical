638d2efe00afb1721e94d744826315184ed00aa7
joeCodes05 <nathancodes05@gmail.com>
2023-11-05 21:32:55 -1100

    update js db file

---
+++ src/data/aboutSales.js
@@ -0,0 +1,32 @@
+export const aboutSalesText = [
+  {
+    id: 1,
+    title: "Reliability",
+    details:
+      "Our track record speaks for itself. With a commitment to on-time deliveries and secure handling, your cargo is in safe hands.",
+  },
+  {
+    id: 2,
+    title: "Innovation",
+    details:
+      "In a rapidly evolving industry, we stay ahead of the curve by adopting cutting-edge technologies and creative solutions that streamline your supply chain.",
+  },
+  {
+    id: 3,
+    title: "Global Reach",
+    details:
+      "No destination is too far. Our extensive network of partners and agents spans the globe, ensuring your goods reach their destination, no matter where that may be.",
+  },
+  {
+    id: 4,
+    title: "Transparency",
+    details:
+      "We believe in open communication. Throughout the journey, you’ll have real-time access to tracking and updates, giving you peace of mind.",
+  },
+  {
+    id: 5,
+    title: "Sustainability",
+    details:
+      "Responsibility is at the heart of what we do. We’re committed to reducing our environmental footprint and promoting eco-friendly practices.",
+  },
+];
